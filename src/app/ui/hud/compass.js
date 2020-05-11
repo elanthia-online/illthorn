07059daf2b7b70480c2a6407c7cecb2001e9deef
Benjamin Clos <benjamin.clos@gmail.com>
2020-05-11 14:34:26 -0400

    feat: add better compass

--- src/app/ui/hud/compass.js
+++ src/app/ui/hud/compass.js
@@ -6,27 +6,33 @@ const Progress  = require("../progress")
 
 module.exports = class Compass {
   static DIRS =
-    [ "", "up", ""
-    , "nw", "n", "ne", 
-    , "w" , "out", "e",
-    , "sw", "s", "se", 
-    , "", "down", ""
+    [ ""  , "up",   ""
+    , "nw", "n",    "ne" 
+    , "w" , "out",  "e"
+    , "sw", "s",    "se" 
+    , ""  , "down", ""
     ]
+
+  static MAP = 
+    { up   : "u"
+    , down : "d"
+    , out  : "o"
+    }
 
   static current () {
     return Lens.get(Session.current, "state.compass.children", [])
       .map(dir => Lens.get(dir, "attrs.value"))
       .filter(dir => typeof dir == "string")
-      .reduce((acc, dir) => Object.assign(acc, {[dir]: 1}), {})
+      .reduce((acc, dir) => {
+        return Object.assign(acc, {[dir]: 1})
+      }, {})
   }
   
   view () {
     const available_dirs = Compass.current()
 
-    //console.log(available_dirs)
-
     return m(Panel, {id: "compass", title: "compass"}, 
       m("ul", Compass.DIRS.map(dir => m("li", 
-        {class: available_dirs[dir] ? "on" : "off"}, dir))))
+        {class: available_dirs[dir] ? "on" : "off"}, Compass.MAP[dir] || dir))))
   }
 }

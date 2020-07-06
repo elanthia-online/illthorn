8195aaa39bd4ed06f5bf6adacfa876140686e081
Chris Coyier <chriscoyier@gmail.com>
2020-07-06 11:19:54 -0700

    Optional Right Column

--- src/app/ui/index.js
+++ src/app/ui/index.js
@@ -2,3 +2,6 @@ exports.CLI = require("./cli")
 exports.Sessions = require("./sessions")
 exports.Hands = require("./hands")
 exports.HUD = require("./hud")
+
+const InitLayout = require("./layout")
+InitLayout()

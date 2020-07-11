e58593d73e7c99d8a470a70be6acc4321ffa992e
Chris Coyier <chriscoyier@gmail.com>
2020-07-11 15:08:02 -0700

    correct validation

--- src/vimish/index.js
+++ src/vimish/index.js
@@ -329,10 +329,7 @@ exports.stream = exports.streams = Command.of(
 
 // `:theme dark-king`
 exports.theme = Command.of(["value"], async ({ value }) => {
-  if (
-    value === THEME_NAMES.original ||
-    value === THEME_NAMES["dark-king"]
-  ) {
+  if (value in THEME_NAMES) {
     Settings.set("theme", value)
     Bus.emit(Bus.events.CHANGE_THEME, {
       theme: value,

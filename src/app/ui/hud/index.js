c5101030bf75fc9d47ca83a513c8a952f990ca8b
Benjamin Clos <benjamin.clos@gmail.com>
2021-09-13 11:01:15 -0600

    feat: add support for Effects;

--- src/app/ui/hud/index.js
+++ src/app/ui/hud/index.js
@@ -10,14 +10,11 @@ module.exports = class HUD {
     return [
       Settings.get("vitals", true) && m(Vitals),
       Settings.get("injuries", true) && m(Injuries),
-      Settings.get("active-spells", true) &&
+      Settings.get("effects", true) &&
         m(Effects, { dialog: "Active Spells" }),
-      Settings.get("active-spells", true) &&
-        m(Effects, { dialog: "Cooldowns" }),
-      Settings.get("active-spells", true) &&
-        m(Effects, { dialog: "Buffs" }),
-      Settings.get("active-spells", true) &&
-        m(Effects, { dialog: "Debuffs" }),
+      Settings.get("effects", true) && m(Effects, { dialog: "Cooldowns" }),
+      Settings.get("effects", true) && m(Effects, { dialog: "Buffs" }),
+      Settings.get("effects", true) && m(Effects, { dialog: "Debuffs" }),
       Settings.get("compass", true) && m(Compass),
     ]
   }
